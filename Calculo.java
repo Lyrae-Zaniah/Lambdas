0ab873fdc2032e149c7af1f79c6585d5bd9dbb4a
Guilherme <guilhermepolaski@protonmail.com>
2025-09-10 18:45:09 -0300

    Modificado Arquivos

--- Calculo.java
+++ Calculo.java
@@ -3,5 +3,9 @@ package Lambdas;
 @FunctionalInterface
 public interface Calculo {
 
-    public abstract double executar(double a, double b);
+    double executar(double a, double b);
+    
+    default String legal() {
+        return "Legal";
+    }
 }
